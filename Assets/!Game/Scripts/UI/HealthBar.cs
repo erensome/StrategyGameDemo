d51c6f86ec812bd0bf5dfefb9511acc4b13d4ec1
erensome <erensome1@gmail.com>
2025-04-26 00:46:43 +0300

    refactor healthbar
    event driven system
    animation with dofade

--- Assets/!Game/Scripts/UI/HealthBar.cs
+++ Assets/!Game/Scripts/UI/HealthBar.cs
@@ -1,4 +1,4 @@
-using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,24 +6,55 @@ public class HealthBar : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Slider slider;
+    [SerializeField] private CanvasGroup canvasGroup;
     [SerializeField] private Image fillImage;
     [SerializeField] private Gradient gradient;
 
+    private HealthComponent healthComponent;
+    
     private void Start()
     {
+        healthComponent = GetComponentInParent<HealthComponent>();
+        if (healthComponent != null)
+        {
+            SetMaxHealth(healthComponent.MaxHealth);
+            healthComponent.OnHealthChanged += UpdateHealthBar;
+            healthComponent.OnDeath += HandleDeath;
+        }
+        
         transform.localPosition = new Vector3(0, 0.55f, 0);
     }
-
-    public void SetHealth(float health)
-    {
-        slider.value = health;
-        fillImage.color = gradient.Evaluate(slider.normalizedValue);
-    }
     
-    public void SetMaxHealth(float maxHealth)
+    private void SetMaxHealth(float maxHealth)
     {
         slider.maxValue = maxHealth;
         slider.value = maxHealth;
         fillImage.color = gradient.Evaluate(1f);
+    }
+
+    private void UpdateHealthBar(float oldHealth, float newHealth)
+    {
+        slider.value = newHealth;
+        fillImage.color = gradient.Evaluate(slider.normalizedValue);
+    }
+    
+    private void HandleDeath()
+    {
+        slider.value = 0;
+        
+        canvasGroup.DOFade(0, 0.5f)
+            .OnComplete(() =>
+            {
+                Destroy(gameObject);
+            });
+    }
+    
+    protected virtual void OnDestroy()
+    {
+        if (healthComponent != null)
+        {
+            healthComponent.OnHealthChanged -= UpdateHealthBar;
+            healthComponent.OnDeath -= HandleDeath;
+        }
     }
 }
