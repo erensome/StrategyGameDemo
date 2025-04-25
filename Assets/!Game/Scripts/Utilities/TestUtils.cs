15fd285e88688ecfc7c6c60d316ed4182319d256
erensome <erensome1@gmail.com>
2025-04-26 00:51:56 +0300

    add test utility script for world texts

---
+++ Assets/!Game/Scripts/Utilities/TestUtils.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public static class TestUtils // For test
+{
+    public static TextMesh CreateWorldText(string text, Transform parent = null, Vector3 localPosition = default,
+        Color? color = null, TextAnchor textAnchor = TextAnchor.MiddleCenter, int fontSize = 80, int sortingOrder = 0)
+    {
+        color ??= Color.white;
+        return CreateWorldText(parent, text, localPosition, (Color)color, textAnchor, fontSize, sortingOrder);
+    }
+
+    private static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, Color color,
+        TextAnchor textAnchor, int fontSize, int sortingOrder)
+    {
+        GameObject gameObject = new GameObject("World Text", typeof(TextMesh));
+        Transform transform = gameObject.transform;
+        transform.SetParent(parent);
+        transform.localPosition = localPosition;
+        transform.localScale = Vector3.one * 0.05f;
+
+        TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.anchor = textAnchor;
+        textMesh.text = text;
+        textMesh.color = color;
+        textMesh.fontSize = fontSize;
+        textMesh.GetComponent<Renderer>().sortingOrder = sortingOrder;
+
+        return textMesh;
+    }
+}
