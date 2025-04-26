3838f99181fed57595225ecb63c5233921a8c326
erensome <erensome1@gmail.com>
2025-04-26 23:06:08 +0300

    Update Soldier.cs

--- Assets/!Game/Scripts/Soldier.cs
+++ Assets/!Game/Scripts/Soldier.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(HealthComponent))]
 [RequireComponent(typeof(AttackerComponent))]
 [RequireComponent(typeof(SelectableComponent))]
+[RequireComponent(typeof(MovableComponent))]
 public class Soldier : MonoBehaviour, IPoolable
 {
     [Header("Data")]
@@ -12,15 +13,41 @@ public class Soldier : MonoBehaviour, IPoolable
     [SerializeField] private HealthComponent healthComponent;
     [SerializeField] private AttackerComponent attackerComponent;
     [SerializeField] private SelectableComponent selectableComponent;
+    [SerializeField] private MovableComponent movableComponent;
+    
+    [Header("References")]
+    [SerializeField, Tooltip("Visual parent, using for look at rotations")]
+    private Transform visualTransform;
     
     private void Awake()
     {
-        if (healthComponent == null)
-        {
-            healthComponent = GetComponent<HealthComponent>();
-        }
-        
         healthComponent.MaxHealth = unitData.Health;
+        healthComponent.OnDeath += HandleDeath;
+        
+        attackerComponent.AttackDamage = unitData.Damage;
+        attackerComponent.OnAttack += HandleAttack;
+        movableComponent.OnTargetPositionChanged += HandleTargetPositionChanged;
+    }
+
+    private void HandleDeath()
+    {
+        ObjectPoolManager.Instance.ReturnObjectToPool(PoolType.Soldier, gameObject);
+    }
+    
+    private void HandleAttack(IAttacker attacker, IDamageable target)
+    {
+        var targetObject = target as MonoBehaviour;
+        if (targetObject == null) return;
+        
+        Vector3 targetPosition = targetObject.transform.position;
+        Vector3 direction = targetPosition - transform.position;
+        direction.z = 0;
+        visualTransform.right = direction.normalized;
+    }
+    
+    private void HandleTargetPositionChanged(Vector3 targetPosition)
+    {
+        visualTransform.right = (targetPosition - transform.position).normalized;
     }
     
     #region IPoolable
