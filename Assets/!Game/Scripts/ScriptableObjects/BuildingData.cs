d898d04be89090cd6b56a4aeedc21e5f658b20a4
erensome <erensome1@gmail.com>
2025-04-28 23:49:17 +0300

    soldier entity data and bugfix building data

--- Assets/!Game/Scripts/ScriptableObjects/BuildingData.cs
+++ Assets/!Game/Scripts/ScriptableObjects/BuildingData.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "BuildingData", menuName = "ScriptableObjects/GameData/BuildingData", order = 2)]
 public class BuildingData : EntityData
 {
     public int Health;
-    public Image Icon;
+    public Sprite Icon;
     public GameObject Prefab;
 }
