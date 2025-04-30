046403de6c1bdff329f6c812d55ef8b1e312d72f
erensome <erensome1@gmail.com>
2025-04-30 22:43:28 +0300

    add camera controller script

---
+++ Assets/!Game/Scripts/Core/CameraController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float zoomSpeed = 15f;
+    [SerializeField, Tooltip("x value must be min and y value must be max")]
+    private Vector2 orthographicSizeBounds = new Vector2(3f, 25f);
+    
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        if (mainCamera == null)
+        {
+            Debug.LogError("Main camera not found.");
+            return;
+        }
+    }
+
+    public void Move(float horizontalInput, float verticalInput)
+    {
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0).normalized;
+        transform.position += direction * (moveSpeed * Time.deltaTime);
+    }
+
+    // To zoom in orthographic size will be decreased
+    public void ZoomIn()
+    {
+        float newSize = mainCamera.orthographicSize;
+        newSize -= zoomSpeed * Time.deltaTime;
+        newSize = Mathf.Clamp(newSize, orthographicSizeBounds.x, orthographicSizeBounds.y);
+        mainCamera.orthographicSize = newSize;
+    }
+    
+    // To zoom in orthographic size will be decreased
+    public void ZoomOut()
+    {
+        float newSize = mainCamera.orthographicSize;
+        newSize += zoomSpeed * Time.deltaTime;
+        newSize = Mathf.Clamp(newSize, orthographicSizeBounds.x, orthographicSizeBounds.y);
+        mainCamera.orthographicSize = newSize;
+    }
+}
